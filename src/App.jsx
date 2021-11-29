691f73d502a88bc52830c407700dd2b819e0b9d1
Somenath <somenathchoudhury35@gmail.com>
2021-11-29 13:55:04 +0530

    commited

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Clock from "./Clock";
+
+const App = () => {
+    return(<Clock />);
+};
+
+export default App;
---
+++ src/Clock.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const Clock = () => {
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCTime] = useState(time);
+
+  const UpdateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCTime(time);
+  };
+
+  setInterval(UpdateTime, 1000); // setInterval() is a function which will update the time automatically after every 1sec(1sec = 1000ms).
+
+  // The setInterval() method in JavaScript is used to repeat a specified function at every given time-interval.
+  //It evaluates an expression or calls a function at given intervals. This method continues the calling of function until the window is closed
+  //or the clearInterval() method is called. This method returns a numeric value or a non-zero number that identifies the created timer.
+  return (
+    <>
+      <h1>{ctime}</h1>
+    </>
+  );
+};
+
+export default Clock;
